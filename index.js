e37f8d0c4d13ee81075419aaa38975fa09383325
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-31 16:41:30 -0700

    updated

---
+++ index.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
+
+exports.handler = (event, context, callback) => {
+
+  const params = {
+    TableName: 'EVENT_DETAILS',
+    KeyConditionExpression: "#event_id = :event_id",
+    ExpressionAttributeValues: {
+      ":event_id": event.event_id
+    }
+  };
+
+  docClient.query(params, function (err, data) {
+
+    let recordReturned = data.Items[0];
+
+    if (err || (recordReturned.password !== event.password)) {
+
+      data = {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+          message: JSON.stringify('ERROR, event not found.')
+        }
+      };
+      callback(null, data);
+
+    } else {
+
+      data = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+
+          message: JSON.stringify('Successful get.'),
+          event: recordReturned
+        }
+
+      };
+
+      callback(null, data);
+
+    }
+  })
+};
